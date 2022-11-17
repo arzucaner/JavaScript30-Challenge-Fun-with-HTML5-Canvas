96d8d2037f435e37a5d376410f21f2867a8031e6
Arzu Caner <arzuguneycaner@gmail.com>
2022-11-17 23:43:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+
+function draw(e) {
+  if(!isDrawing) return; // stop the fn from running when they are not moused down
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  //go to
+  ctx.LineTo(e.offsetX, e.offsetY);
+  ctx.strokeStyle();
+  lastX = e.offsetX;
+  lastY =e.offsetY;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
